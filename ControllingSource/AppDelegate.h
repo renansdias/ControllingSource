6b5aa60841ef8e4dbda9dccd6aacf12e018eb7fc
Renan Dias <renansdiaz@gmail.com>
2014-06-20 20:09:54 +0100

    Initial Commit

---
+++ ControllingSource/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ControllingSource
+//
+//  Created by Renan Dias on 20/06/2014.
+//  Copyright (c) 2014 Renan Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ControllingSourceTests/ControllingSourceTests.m
@@ -0,0 +1,34 @@
+//
+//  ControllingSourceTests.m
+//  ControllingSourceTests
+//
+//  Created by Renan Dias on 20/06/2014.
+//  Copyright (c) 2014 Renan Dias. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ControllingSourceTests : XCTestCase
+
+@end
+
+@implementation ControllingSourceTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
